08f2da9adae8935e6df63eb4442cd4257b409808
pallab-gain <pallab.gain@gmail.com>
2015-08-05 15:34:38 +0600

    add android openssl x86 patch

---
+++ build_openssl.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+src_root=`pwd`
+NDK=/home/pallab/Documents/IDE/Sdk/ndk-bundle
+
+function build_openssl () {
+  echo "Cloning openssl-android ..."
+  test -d ${src_root}/openssl-android || \
+  git clone https://github.com/pallab-gain/openssl-android.git ${src_root}/openssl-android 
+}
+function apply_patch(){
+  echo "Applying patch for x86 android build ..."
+  patch -u ${src_root}/openssl-android/jni/Application.mk ${src_root}/patches/ssl_x86.patch 
+}
+
+build_openssl
+apply_patch
